13e306f1b8036cd6e9a070577581c9b217de3ce3
DaanF1 <daanstander@hotmail.nl>
2024-06-06 21:20:07 +0200

    Added day/night cycle
    Zwevende belg now has a day/night cycle.
    
    TODO:
    -bug fixing (collision)
    -bug fixing (spawning pipes)
    -menu popup with an exit game button
    -adjusting the difficulty

--- app/src/main/java/com/example/magicarcade/ZwevendeBelg/ZwevendeBelgGameView.java
+++ app/src/main/java/com/example/magicarcade/ZwevendeBelg/ZwevendeBelgGameView.java
@@ -5,18 +5,14 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
+import androidx.core.content.ContextCompat;
 import com.example.magicarcade.R;
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
 
 public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
     // Sprites have a width of 16 and a height of 10
@@ -24,7 +20,7 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
     private Paint paint;
     private SurfaceHolder holder;
     private volatile boolean running = false;
-    private Bitmap birdDown, birdUp, birdStationary;
+    private Bitmap belgDown, belgUp, belgStationary;
     private Bitmap pipeTopBack, pipeTopMiddle, pipeTopFront, pipeBottomBack, pipeBottomMiddle, pipeBottomFront;
     private Bitmap currentBird;
     private int imageHeight;
@@ -33,12 +29,14 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
     private int screenWidth;
     private int screenHeight;
     private int belgX, belgY;
-    private int belgSpeedY;
+    private double belgSpeedY;
     private int pipeX, pipeY;
     private int pipeSpeedX;
     private int prevPipeSpawnTime;
     private int pipeSpawnTime;
+    private int dayNightCycle;
     private HashMap<Integer, PipePart> idPipes = new HashMap<>();
+    private Color theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.lightBlue));
 
     public ZwevendeBelgGameView(Context context) {
         // Configure game settings
@@ -46,11 +44,11 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
         holder = getHolder();
         paint = new Paint();
 
-        birdDown = BitmapFactory.decodeResource(getResources(), R.drawable.belg_down);
-        birdUp = BitmapFactory.decodeResource(getResources(), R.drawable.belg_up);
-        birdStationary = BitmapFactory.decodeResource(getResources(), R.drawable.belg_stationary);
+        belgUp = BitmapFactory.decodeResource(getResources(), R.drawable.belg_up);
+        belgStationary = BitmapFactory.decodeResource(getResources(), R.drawable.belg_stationary);
+        belgDown = BitmapFactory.decodeResource(getResources(), R.drawable.belg_down);
 
-        currentBird = birdStationary;
+        currentBird = belgStationary;
         belgX = 100;
         belgY = 100;
         belgSpeedY = 0;
@@ -58,11 +56,12 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
         imageWidth = 30;
         imageHeight = 20;
         screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-        screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels-110;
         gapHeight = 500;
 
         pipeSpawnTime = 1000;
         prevPipeSpawnTime = pipeSpawnTime;
+        dayNightCycle = 0;
 
         pipeTopBack = BitmapFactory.decodeResource(getResources(), R.drawable.car_top_back_red);
         pipeTopMiddle = BitmapFactory.decodeResource(getResources(), R.drawable.car_top_middle_red);
@@ -73,7 +72,7 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
         pipeBottomBack = BitmapFactory.decodeResource(getResources(), R.drawable.car_bottom_back_red);
 
         pipeX = screenWidth+100;
-        pipeY = 300;
+        pipeY = (int) (Math.random() * (screenHeight-(gapHeight+(imageHeight*2))) + (imageHeight*2));
         pipeSpeedX = -5;
     }
 
@@ -100,70 +99,30 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
     }
 
     private void update() {
-        // Set current state of belg
-        belgY += belgSpeedY;
-        belgSpeedY += 2;
-        if (belgSpeedY > 0) {
-            currentBird = birdDown;
-        } else if (belgSpeedY < 0) {
-            currentBird = birdUp;
-        }
-
-        // Check collision
-        if (belgX >= pipeX){
-            if (belgY + currentBird.getHeight() < pipeY ||
-                    belgY - currentBird.getHeight() < pipeY + gapHeight) {
-                running = false;
-            }
-        }
-
-        // Generate new pipe images
-        int id = 0;
-        int heightCounter = 0;
-
-        PipePart topPart = new PipePart(pipeTopBack, pipeX, heightCounter);
-        idPipes.put(id, topPart);
-        id++;
-        while (heightCounter + imageHeight < pipeY){
-            PipePart topMiddlepart = new PipePart(pipeTopMiddle, pipeX, heightCounter + imageHeight);
-            idPipes.put(id, topMiddlepart);
-            id++;
-            heightCounter += imageHeight;
-        }
-        PipePart topBottomPart = new PipePart(pipeTopFront, pipeX, heightCounter + imageHeight);
-        idPipes.put(id, topBottomPart);
-        id++;
-
-        PipePart bottomTopPart = new PipePart(pipeBottomFront, pipeX, heightCounter + imageHeight + gapHeight);
-        idPipes.put(id, bottomTopPart);
-        id++;
-        while (heightCounter + imageHeight + gapHeight + imageHeight < screenHeight){
-            PipePart bottomMiddlePart = new PipePart(pipeBottomMiddle, pipeX, heightCounter + imageHeight + gapHeight + imageHeight);
-            idPipes.put(id, bottomMiddlePart);
-            id++;
-            heightCounter += imageHeight;
-        }
-        PipePart bottomBottomPart = new PipePart(pipeBottomBack, pipeX - 50, heightCounter + imageHeight + gapHeight + imageHeight - 100);
-        idPipes.put(id, bottomBottomPart);
+        setBelgImage();
+        checkCollision();
+        generatePipeImages();
+        setDayNightCycle();
 
         // Timers and adjustable values
         pipeX += pipeSpeedX;
-        pipeSpawnTime-=1;
         if (pipeSpawnTime <= 0 || pipeX <= -pipeTopMiddle.getWidth()){
             pipeX = getWidth();
-            pipeY = (int) (Math.random() * getHeight()) + 100;
+            pipeY = (int) (Math.random() * (screenHeight-(gapHeight+(imageHeight*2))) + (imageHeight*2));
 
             // Make game more difficult
             pipeSpeedX += 1;
             gapHeight -= 5;
             pipeSpawnTime = prevPipeSpawnTime-10;
         }
+        pipeSpawnTime-=1;
     }
 
     public void draw(Canvas canvas) {
         super.draw(canvas);
         if (canvas != null){
-            canvas.drawColor(0xFF87CEEB);
+            // Colors (light to dark)
+            canvas.drawColor(theColor.toArgb());
 
             // Draw belg
             canvas.drawBitmap(currentBird, belgX, belgY, paint);
@@ -179,7 +138,7 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             belgSpeedY = -30;
-            currentBird = birdUp;
+            currentBird = belgUp;
         }
         return true;
     }
@@ -201,6 +160,92 @@ public class ZwevendeBelgGameView extends SurfaceView implements Runnable {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void setBelgImage(){
+        // Set current state of belg
+        belgY += belgSpeedY;
+        belgSpeedY += 2;
+        if (belgSpeedY > -5 && belgSpeedY < 5){
+            currentBird = belgStationary;
+        } else if (belgSpeedY >= 5) {
+            currentBird = belgDown;
+        } else if (belgSpeedY <= -5) {
+            currentBird = belgUp;
+        }
+    }
+
+    public void checkCollision(){
+        // Check collision
+        if (belgY <= 0 || belgY >= screenHeight){
+            running = false;
+        }
+        if (belgX >= pipeX){
+            if (belgY + currentBird.getHeight() > pipeY + gapHeight ||
+                    belgY - currentBird.getHeight() < pipeY) {
+                running = false;
+            }
+        }
+    }
+
+    public void generatePipeImages(){
+        // Generate new pipe images
+        int id = 0;
+        int heightCounter = 0;
+
+        PipePart topPart = new PipePart(pipeTopBack, pipeX, heightCounter);
+        idPipes.put(id, topPart);
+        id++;
+        while (heightCounter + imageHeight < pipeY - imageHeight){
+            heightCounter += imageHeight;
+            PipePart topMiddlepart = new PipePart(pipeTopMiddle, pipeX, heightCounter);
+            idPipes.put(id, topMiddlepart);
+            id++;
+        }
+        PipePart topBottomPart = new PipePart(pipeTopFront, pipeX, heightCounter + imageHeight);
+        idPipes.put(id, topBottomPart);
+        id++;
+
+        // PipeY value is here, at the bottom of the top pipe
+        PipePart bottomTopPart = new PipePart(pipeBottomFront, pipeX, heightCounter + gapHeight);
+        idPipes.put(id, bottomTopPart);
+        id++;
+        while (heightCounter + imageHeight + gapHeight < screenHeight + 20){
+            heightCounter += imageHeight;
+            PipePart bottomMiddlePart = new PipePart(pipeBottomMiddle, pipeX, heightCounter + gapHeight);
+            idPipes.put(id, bottomMiddlePart);
+            id++;
+        }
+        PipePart bottomBottomPart = new PipePart(pipeBottomBack, pipeX, heightCounter + imageHeight + gapHeight);
+        idPipes.put(id, bottomBottomPart);
+    }
+
+    public void setDayNightCycle(){
+        // Day / Nigh cycle
+        if (dayNightCycle >= 3000){
+            dayNightCycle = 1;
+        } else if (dayNightCycle > 0 && dayNightCycle <= 1050) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.lightBlue));
+        } else if (dayNightCycle > 1050 && dayNightCycle <= 1150) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.lightDarkerBlue));
+        } else if (dayNightCycle > 1150 && dayNightCycle <= 1250) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.blue));
+        } else if (dayNightCycle > 1250 && dayNightCycle <= 1350) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.goingDarkBlue));
+        } else if (dayNightCycle > 1350 && dayNightCycle <= 1450) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.almostDarkBlue));
+        } else if (dayNightCycle > 1550 && dayNightCycle <= 2600) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.darkBlue));
+        } else if (dayNightCycle > 2600 && dayNightCycle <= 2700) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.almostDarkBlue));
+        } else if (dayNightCycle > 2700 && dayNightCycle <= 2800) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.goingDarkBlue));
+        } else if (dayNightCycle > 2800 && dayNightCycle <= 2900) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.blue));
+        } else if (dayNightCycle > 2900 && dayNightCycle < 3000) {
+            theColor = Color.valueOf(ContextCompat.getColor(getContext(), R.color.lightDarkerBlue));
+        }
+        dayNightCycle+=1;
     }
 
 
